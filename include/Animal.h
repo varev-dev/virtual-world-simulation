8476f0f45c72caf7264f626ec38a94d9f68da48a
Kacper Doga <doga.kacper@gmail.com>
2024-04-22 00:51:51 +0200

    animal collision

--- include/Animal.h
+++ include/Animal.h
@@ -9,6 +9,8 @@
 #include "Plant.h"
 
 class Animal : public Organism {
+private:
+    uint32_t lastActionTurn = 0;
 public:
     Animal(uint16_t x, uint16_t y, uint8_t power, uint8_t initiative, World* world);
     void action() override;
--- src/Animal.cpp
+++ src/Animal.cpp
@@ -12,41 +12,8 @@ Animal::Animal(uint16_t x, uint16_t y, uint8_t power, uint8_t initiative, World 
     Organism::sign = 'A';
 }
 
-direction randomizeDirection(const Organism& organism) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, 3);
-    bool isMoveValid = false;
-    direction dir;
-    while (!isMoveValid) {
-        dir = direction(dis(gen));
-        switch (dir) {
-            case NORTH:
-                if (organism.getY() != 0)
-                    isMoveValid = true;
-                break;
-            case SOUTH:
-                if (organism.getY() != organism.getWorld()->getHeight() - 1)
-                    isMoveValid = true;
-                break;
-            case WEST:
-                if (organism.getX() != 0)
-                    isMoveValid = true;
-                break;
-            case EAST:
-                if (organism.getX() != organism.getWorld()->getWidth() - 1)
-                    isMoveValid = true;
-                break;
-            default:
-                std::cout << "Unknown direction";
-                break;
-        }
-    }
-    return dir;
-}
-
 void Animal::action() {
-    direction dir = randomizeDirection(*this);
+    direction dir = getRandomDirection();
     uint16_t* position = newPosition(dir);
 
     Organism* collider;
@@ -65,5 +32,8 @@ void Animal::action() {
 }
 
 void Animal::collision(Organism &organism) {
-
+    if (power <= organism.getPower())
+        world->removeOrganism(*this);
+    else
+        world->removeOrganism(organism);
 }
